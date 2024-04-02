643186693f4f9eb5cb19ef80a2568d625d4c5a60
Devanshu046 <dev44908@gmail.com>
2024-04-02 12:14:42 +0530

    useEffectHook

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css';
-import HookCounterFour from './component/HookCounterFour';
-
+import DataFatching from './component/DataFatching';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour/>
+      <DataFatching/>
     </div>
   );
 }
