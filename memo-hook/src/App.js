ca3c71cb10583ddc381ac34a9a4a9814dc121565
Devanshu046 <dev44908@gmail.com>
2024-04-07 11:32:16 +0530

    customHook

--- memo-hook/src/App.js
+++ memo-hook/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import ClassTimer from './useRefComp/ClassTimer';
-import HookTimer from './useRefComp/HookTimer';
 
 
 function App() {
   return (
     <div className="App">
-      <ClassTimer/>
-     <HookTimer/>
     </div>
   );
 }
